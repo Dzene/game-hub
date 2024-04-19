25f913a0a26c683d0b9a6d1597ec451e082b50d6
Dzene <indzicdzeneta98@gmail.com>
2024-04-19 22:24:42 +0200

    Fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,34 +1,35 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Game } from '../hooks/useGames'
-import getCroppedImageUrl from '../services/image-url'
-import CriticScore from './CriticScore'
-import Emoji from './Emoji'
-import PlatformIconList from './PlatformIconList'
+import { Game } from "../entities/Game";
+import getCroppedImageUrl from "../services/image-url";
+import CriticScore from "./CriticScore";
+import Emoji from "./Emoji";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
-  game: Game
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   const navigate = useNavigate();
   return (
-    <Card
-      onClick={()=>navigate('/games/' + game.slug)}
-    >
+    <Card onClick={() => navigate("/games/" + game.slug)}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <HStack justifyContent='space-between' marginBottom={3}>
-          <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms?.map((p) => p.platform)}
+          />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize='2xl'>
-          <Link to={'/games/' + game.slug}>{game.name}</Link>
-        <Emoji rating={game.rating_top}/></Heading>
+        <Heading fontSize="2xl">
+          <Link to={"/games/" + game.slug}>{game.name}</Link>
+          <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
