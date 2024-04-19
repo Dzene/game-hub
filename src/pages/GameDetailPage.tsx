c925398ac41d5cd92369919cc7db6b26e0c23f68
Dzene <indzicdzeneta98@gmail.com>
2024-04-19 22:20:31 +0200

    Refactor- Move entity interfaces

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,12 +1,10 @@
-import { Heading, Spinner, Text } from '@chakra-ui/react';
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import useGame from '../hooks/useGame';
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
+import { Heading, Spinner, Text } from "@chakra-ui/react";
 
 const GameDetailPage = () => {
-  const {slug} = useParams();
-  const {data: game, isLoading, error } = useGame(slug!);
-
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!);
   if (isLoading) return <Spinner />;
 
   if (error || !game) throw error;
@@ -16,7 +14,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <Text>{game.description_raw}</Text>
     </>
-  )
-}
+  );
+};
 
-export default GameDetailPage
+export default GameDetailPage;
